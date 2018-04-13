fbfa1d44e4e6faa7ebfc1745072fa726522aabce
Casey Trimm <me@caseytrimm.com>
2018-04-13 17:29:26 -0400

    Ignore release dir, update webpack to support release, add generate-package-json-webpack-plugin

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -11,9 +11,21 @@
 // var webpack = require('webpack');
 // var path = require('path');
 const fs = require('fs');
+const GeneratePackageJsonPlugin = require('generate-package-json-webpack-plugin');
+
+const PACKAGE_DIR = __dirname + "/../package.json";
+const PACKAGE = require(PACKAGE_DIR);
+const RELEASE = process.argv.includes('--release');
+
+const buildPackage = {
+  "name": PACKAGE.name,
+  "version": PACKAGE.version,
+  "private": PACKAGE.private,
+  "bundledDependencies": Object.keys(PACKAGE.dependencies)
+};
+
 
 const nodeModules = {};
-
 fs.readdirSync('./node_modules')
   .filter(x => ['.bin'].indexOf(x) === -1)
   .forEach(mod => (nodeModules[mod] = `commonjs ${mod}`));
@@ -22,13 +34,12 @@ module.exports = {
   entry: './src/app.js',
   target: 'node',
   output: {
-    path: './build',
-    filename: 'daemon.js',
+    path: RELEASE ? './release' : './build',
+    filename: 'server.js',
   },
   externals: nodeModules,
   module: {
     loaders: [
-      //     { test: /\.css$/, loader: "style!css" }
       {
         test: /\.js$/,
         exclude: [/node_modules/],
