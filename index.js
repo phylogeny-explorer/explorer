da8e7ebad1ae46f6f96a24f60d9ae20793b78a88
Casey Trimm <me@caseytrimm.com>
2018-04-17 23:14:12 -0400

    MOD export AWS from index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import databases from './databases';
+import aws from './aws';
 import * as config from './config';
 
 export const Database = databases;
 export const Config = config;
+export const AWS = aws;
