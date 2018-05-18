652d6f2bad9dc3bf14d07441bd125dcb1c2d9d98
Casey Trimm <me@caseytrimm.com>
2018-05-18 09:15:05 -0400

    MOD add max depth of 6 MOD remove unneeded generate function

--- src/controllers/clade.js
+++ src/controllers/clade.js
@@ -22,7 +22,7 @@ class CladeController extends Controller {
 
   getClades(req, res, next) {
     const nodeId = req.params.id || '55ae8ce9343108fa191058d2'; // Root node
-    const depth = Number.parseInt(req.params.depth, 10) || 3;
+    const depth = Math.min(Number.parseInt(req.params.depth, 10) || 3, 6);
     Clade.findById(nodeId, '-__v', (err, clade) => {
       if (clade) {
         const tree = new Tree(Clade, clade.toObject(), depth, result =>
@@ -58,55 +58,6 @@ class CladeController extends Controller {
     const self = req.body.self;
     Clade.find({ name: { $regex: `^${name}`, $options: 'i' }, _id: { $ne: self } }, 'name',
       (err, clades) => this.handleResponse(res, next, err, clades));
-  }
-
-  generate(req, res, next) {
-    const localConn = mongoose.createConnection('mongodb://localhost:27017/phylex-public');
-    const connectionString = 'mongodb://35.162.254.17:27017/phylex-public';
-
-    const options = {
-      user: 'phylexpublicuser',
-      pass: '53010c7ca48711e680f576304dec7eb7',
-    };
-
-    const remoteConn = mongoose.createConnection(connectionString, options);
-    const localClade = localConn.model('clades', Clade.schema);
-    const RemoteClade = remoteConn.model('clades', Clade.schema);
-
-    localClade.find({}, (err, olClades) => {
-      for (let i = 0; i < olClades.length; i += 1) {
-        const o = olClades[i];
-        const clade = new RemoteClade();
-
-        if (o.name !== null) {
-          o.name = o.name.trim();
-          if (o.name.indexOf('DUPLICATE') >= 0) {
-            continue;
-          }
-          if (o.name.indexOf('Paparia') >= 0) {
-            continue;
-          }
-          if (o.name.indexOf('†') >= 0) {
-            o.name = o.name.replace('†', '').trim();
-            o.extant = false;
-          }
-          if (o.name.indexOf('|') >= 0) {
-            o.name = null;
-          }
-        }
-
-        clade._id = o._id;
-        clade.parent = o.parent;
-        clade.name = o.name || null;
-        clade.description = o.description || null;
-        clade.otherNames = o.otherNames || null;
-        clade.extant = o.extant || null;
-        clade.created = Date.now();
-        clade.modified = null;
-        clade.save((err1, data) => console.error(err1, data.name));
-      }
-      this.handleResponse(res, next, err, `Finished ${olClades.length} old clades`);
-    });
   }
 }
 
