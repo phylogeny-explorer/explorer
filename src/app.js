b529db8efd5baf501b9bb17b97a9abb27e968dc6
Casey Trimm <me@caseytrimm.com>
2018-07-27 19:20:59 -0400

    Allow TLS credentials to be passed to api

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', false);
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
