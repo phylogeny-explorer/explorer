a637180cad6b5ef4d803f871b1e695b3613b7dd3
Casey Trimm <me@caseytrimm.com>
2018-07-27 20:51:49 -0400

    FIX set X-Requested-With as access-control-allow-header in order to enable cors with https

--- src/app.js
+++ src/app.js
@@ -30,15 +30,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add headers
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, Authorization, Content-Type, X-Request-URL');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, Authorization, Content-Type, X-Request-URL, X-Requested-With');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
