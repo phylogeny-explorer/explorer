a78662ab2e043884dd9141ff35a8c14ead183cca
Emmanuel Proulx <emmanuel.proulx@dialogic.com>
2018-08-22 12:16:00 -0400

    Formatting

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -12,15 +12,13 @@ import validator from 'validator';
 import passport from 'passport';
 import generator from 'generate-password';
 import { User } from 'common/databases/admin';
-import bcrypt from "bcrypt";
 import { SES } from 'common/aws';
-import mongoose from 'mongoose';
 
 
 const AuthenticationController = {
   /**
    * Validate the sign up form
-   * 
+   *
    * @param {object}
    *          payload - the HTTP body message
    * @returns {object} The result of validation. Object contains a boolean
@@ -40,16 +38,14 @@ const AuthenticationController = {
     if (!payload || typeof payload.password !== 'string' || payload.password.trim().length < 8) {
       isFormValid = false;
       errors.password = 'Password must have at least 8 characters.';
-    }
-    else {
+    } else {
       payload.password = payload.password.trim();
     }
 
     if (!payload || typeof payload.username !== 'string' || payload.username.trim().length === 0) {
       isFormValid = false;
       errors.name = 'Please provide your username.';
-    }
-    else {
+    } else {
       payload.username = payload.username.trim();
     }
 
@@ -66,7 +62,7 @@ const AuthenticationController = {
 
   /**
    * Validate the login form
-   * 
+   *
    * @param {object}
    *          payload - the HTTP body message
    * @returns {object} The result of validation. Object contains a boolean
@@ -81,16 +77,14 @@ const AuthenticationController = {
     if (!payload || typeof payload.username !== 'string' || payload.username.trim().length === 0) {
       isFormValid = false;
       errors.username = 'Please provide your username.';
-    }
-    else {
+    } else {
       payload.username = payload.username.trim();
     }
 
     if (!payload || typeof payload.password !== 'string' || payload.password.trim().length === 0) {
       isFormValid = false;
       errors.password = 'Please provide your password.';
-    }
-    else {
+    } else {
       payload.password = payload.password.trim();
     }
 
@@ -107,7 +101,7 @@ const AuthenticationController = {
 
   /**
    * Validate the forgot form
-   * 
+   *
    * @param {object}
    *          payload - the HTTP body message
    * @returns {object} The result of validation. Object contains a boolean
@@ -122,9 +116,8 @@ const AuthenticationController = {
     if (!payload || typeof payload.username !== 'string' || payload.username.trim().length === 0) {
       isFormValid = false;
       errors.username = 'Please provide your username or email address.';
-    }
-    else {
-        payload.username = payload.username.trim();
+    } else {
+      payload.username = payload.username.trim();
     }
 
     if (!isFormValid) {
@@ -138,54 +131,51 @@ const AuthenticationController = {
     };
   },
   validatePasswordResetForm: (payload) => {
-      const errors = {};
-      let isFormValid = true;
-      let message = '';
-      let p1 = false;
-      let p2 = false;
+    const errors = {};
+    let isFormValid = true;
+    let message = '';
+    let p1 = false;
+    let p2 = false;
 
-      if (!payload || typeof payload.username !== 'string' || payload.username.trim().length === 0) {
-        isFormValid = false;
-        errors.username = 'The username is missing.';
-      }
-      else {
-        payload.username = payload.username.trim();
-      }
+    if (!payload || typeof payload.username !== 'string' || payload.username.trim().length === 0) {
+      isFormValid = false;
+      errors.username = 'The username is missing.';
+    } else {
+      payload.username = payload.username.trim();
+    }
 
     if (!payload || typeof payload.password !== 'string' || payload.password.trim().length === 0) {
-        isFormValid = false;
-        errors.password = 'Please provide a password.';
-      }
-    else {
-        p1 = true;
-        payload.password = payload.password.trim();
-    }
-      
-      if (!payload || typeof payload.repeat_password !== 'string' || payload.repeat_password.trim().length === 0) {
-        isFormValid = false;
-        errors.repeat_password = 'Enter the password a second time.';
-      }
-    else {
-        p2 = true;
-        payload.repeat_password = payload.repeat_password.trim();
+      isFormValid = false;
+      errors.password = 'Please provide a password.';
+    } else {
+      p1 = true;
+      payload.password = payload.password.trim();
     }
 
-      if (p1 && p2 && payload.repeat_password != payload.password) {
-        isFormValid = false;
-        errors.repeat_password = 'The second password does not match the first one.';
-      }
+    if (!payload || typeof payload.repeat_password !== 'string' || payload.repeat_password.trim().length === 0) {
+      isFormValid = false;
+      errors.repeat_password = 'Enter the password a second time.';
+    } else {
+      p2 = true;
+      payload.repeat_password = payload.repeat_password.trim();
+    }
 
-      if (!isFormValid) {
-        message = 'Check the form for errors.';
-      }
+    if (p1 && p2 && payload.repeat_password != payload.password) {
+      isFormValid = false;
+      errors.repeat_password = 'The second password does not match the first one.';
+    }
 
-      return {
-        success: isFormValid,
-        message,
-        errors,
-      };
-    },
-  
+    if (!isFormValid) {
+      message = 'Check the form for errors.';
+    }
+
+    return {
+      success: isFormValid,
+      message,
+      errors,
+    };
+  },
+
   signup: (req, res, next) => {
     const validationResult = AuthenticationController.validateSignupForm(req.body);
     if (!validationResult.success) {
@@ -265,8 +255,7 @@ const AuthenticationController = {
       });
     }
 
-    return User.findOne({ $or: [ {username: req.body.username},{email: req.body.username} ], isActive: true }, (err, user) => {
-
+    return User.findOne({ $or: [{ username: req.body.username }, { email: req.body.username }], isActive: true }, (err, user) => {
       if (err) {
         return next(err);
       }
@@ -274,79 +263,81 @@ const AuthenticationController = {
       if (!user) {
         return res.status(400).json({
           success: false,
-          message: "Can't find username or email. Check the spelling. This user may not exist -- you should register."
+          message: "Can't find username or email. Check the spelling. This user may not exist -- you should register.",
         });
-      }        
-      var resetCode = generator.generate({
-          length: 16,
-          numbers: true,
-          excludeSimilarCharacters: false,
-          symbols: false
+      }
+      const resetCode = generator.generate({
+        length: 16,
+        numbers: true,
+        excludeSimilarCharacters: false,
+        symbols: false,
       });
       user.passwordResetCode = resetCode;
-      let now = new Date();
+      const now = new Date();
       // 30 minutes expiry
-      user.passwordResetExpiry = new Date(now.getTime() + 30*60000); 
+      user.passwordResetExpiry = new Date(now.getTime() + 30 * 60000);
       user.save((err, user) => {
         if (err) {
-          console.log('user update error');
+          console.log('User update error: Can\'t save password reset code');
           return res.status(400).json({
             success: false,
-            message: "Can't save password reset code."
+            message: "Can't save password reset code.",
           });
         }
-        let url = req.headers.referer.substring(0, req.headers.referer.lastIndexOf("/"));
-        url +=  '/passwordReset?resetCode=' + resetCode + '&username=' + user.username;
-        SES.sendEmail(user.email, 
-            "Password Change -- Phylogeny Explorer", 
-            "<HTML><BODY><H1>Choose a new password!</H1><p>You have requested a new password for the account '"+ user.username + "'. To assign new password, click on this link:</p><p><a href='" + url + "'>" + url + "</a></p><p>This link is only valid for 30 minutes from now.</p><p>PS. If you did not request a new password, do not worry; your account is intact and your old password still works.</p><p>Yours truly,</p><p><i>The Phylogeny Explorer</i></p></p></BODY><HTML>");
+        let url = req.headers.referer.substring(0, req.headers.referer.lastIndexOf('/'));
+        url += `/passwordReset?resetCode=${resetCode}&username=${user.username}`;
+        SES.sendEmail(user.email,
+            'Password Change -- Phylogeny Explorer',
+            `<HTML><BODY><H1>Choose a new password!</H1><p>You have requested a new password for the account '${user.username}'. To assign new password, click on this link:</p><p><a href='${url}'>${url}</a></p><p>This link is only valid for 30 minutes from now.</p><p>PS. If you did not request a new password, do not worry; your account is intact and your old password still works.</p><p>Yours truly,</p><p><i>The Phylogeny Explorer</i></p></p></BODY><HTML>`);
         return res.status(200).json({
           success: true,
-          message: "Your temporary password was sent to " + user.email
+          message: `Your temporary password was sent to ${  user.email}`,
         });
       });
     });
   },
   passwordReset: (req, res, next) => {
-      const validationResult = AuthenticationController.validatePasswordResetForm(req.body);
-      if (!validationResult.success) {
-        return res.status(400).json({
-          success: false,
-          message: validationResult.message,
-          errors: validationResult.errors,
-        });
-      }
-      return User.findOne({passwordResetCode: req.body.resetCode , username: req.body.username, passwordResetExpiry: {$gt: Date.now()} , isActive: true }, (err, user) => {
-
+    const validationResult = AuthenticationController.validatePasswordResetForm(req.body);
+    if (!validationResult.success) {
+      return res.status(400).json({
+        success: false,
+        message: validationResult.message,
+        errors: validationResult.errors,
+      });
+    }
+    return User.findOne({
+      passwordResetCode: req.body.resetCode,
+      username: req.body.username,
+      passwordResetExpiry: { $gt: Date.now() }, isActive: true }, (err, user) => {
       if (err) {
-      return next(err);
+        return next(err);
       }
 
       if (!user) {
-      return res.status(400).json({
+        return res.status(400).json({
           success: false,
-          message: "Invalid reset code. This one might be expired. Reset your password again."
+          message: 'Invalid reset code. This one might be expired. Reset your password again.',
         });
-      }        
+      }
       user.password = req.body.password;
       user.passwordResetCode = null;
       user.passwordResetExpiry = null;
-      user.save((err) => {  
+      user.save((err) => {
         if (err) {
           console.log('user update error');
           return res.status(400).json({
             success: false,
-            message: "Can't save password."
+            message: "Can't save password.",
           });
         }
         return res.status(200).json({
           success: true,
-          message: "Your new password is saved.",
+          message: 'Your new password is saved.',
         });
       });
     });
   },
-  logout: (req, res, next) => {
+  logout: () => {
 
   },
 };
