e657750679f47060a466d31de294fade71218f91
Emmanuel Proulx <emmanuel.proulx@dialogic.com>
2018-08-24 00:37:49 -0400

    New email manager

--- aws/index.js
+++ aws/index.js
@@ -1,2 +1,5 @@
 import FileManager from './s3/FileManager';
+import EmailManager from './ses/EmailManager';
+
 export const S3 = new FileManager();
+export const SES = new EmailManager();
--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import Public from './databases/public';
 
 export const Database = { Admin, Public };
 export { S3 } from './aws';
+export { SES } from './aws';
