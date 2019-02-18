58509e404643d768b72efea30f63c6e0a6781d9c
i2xzy <yitzchokr@live.com>
2019-02-18 14:37:51 +0000

    add assertion to clades_info, create clades_update relates #60

--- cypress/integration/clades_info.spec.js
+++ cypress/integration/clades_info.spec.js
@@ -1,9 +1,14 @@
 const localhost_and_port = 'http://localhost:3000';
+const cladeID = '55d5cb8f343108110a2d6f70';
+
 describe('Can view clade info page when', function() {
-  it('logs in and visits /clades/info/...', function() {
+  it('logs in, visits /clades/info/... and return to tree', function() {
     cy.login('admin', 'adminadmin');
     cy.url().should('eq', localhost_and_port + '/clades');
-    cy.visit(localhost_and_port + '/clades/info/55d5cb8f343108110a2d6f70/');
+
+    cy.visit(localhost_and_port + '/clades/info/' + cladeID);
+    cy.url().should('eq', localhost_and_port + '/clades/info/' + cladeID);
+
     cy.get('#returnToTreeButton').click();
     cy.url().should('eq', localhost_and_port + '/clades');
   });
--- cypress/integration/clades_update.spec.js
+++ cypress/integration/clades_update.spec.js
@@ -1,10 +1,29 @@
 const localhost_and_port = 'http://localhost:3000';
+const cladeID = '55d5cb8f343108110a2d6f70';
+
 describe('Can view clade info page when', function() {
-  it('logs in and visits /clades/info/...', function() {
+  it('logs in, visits /clades/update/... and gos back to clade view', function() {
     cy.login('admin', 'adminadmin');
     cy.url().should('eq', localhost_and_port + '/clades');
-    cy.visit(localhost_and_port + '/clades/update/55d5cb8f343108110a2d6f70/');
-    cy.get('#returnToTreeButton').click();
+
+    cy.visit(localhost_and_port + '/clades/update/' + cladeID);
+    cy.url().should('eq', localhost_and_port + '/clades/update/' + cladeID);
+
+    cy.get('#backToViewButton').click();
+    cy.url().should('eq', localhost_and_port + '/clades/info/' + cladeID);
+  });
+
+  it('visits /clades/view/..., clicks edit button and then the cancel button', function() {
+    cy.login('admin', 'adminadmin');
     cy.url().should('eq', localhost_and_port + '/clades');
+
+    cy.visit(localhost_and_port + '/clades/info/' + cladeID);
+    cy.url().should('eq', localhost_and_port + '/clades/info/' + cladeID);
+
+    cy.get('#editButton').click();
+    cy.url().should('eq', localhost_and_port + '/clades/update/' + cladeID);
+
+    cy.get('#cancelButton').click();
+    cy.url().should('eq', localhost_and_port + '/clades/info/' + cladeID);
   });
 });
