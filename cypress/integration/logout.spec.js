3c60051d6f7c8188a0857dd95c7b095c94a73a69
i2xzy <yitzchokr@live.com>
2019-02-25 18:22:26 +0000

    change cy.url to cy.location(pathname), use should(contains) for some assertions #60

--- cypress/integration/logout.spec.js
+++ cypress/integration/logout.spec.js
@@ -1,12 +1,9 @@
-const localhost_and_port = 'http://localhost:3000';
-
 describe('Returns to main page when', function() {
   it('logs in and logs out', function() {
     cy.login('admin', 'adminadmin');
-    cy.url().should('eq', localhost_and_port + '/clades');
-    cy.visit(localhost_and_port + '/auth/logout');
-    cy.url().should('eq', localhost_and_port + '/auth/logout');
-    cy.wait(5000);
-    cy.url().should('eq', localhost_and_port + '/');
+    cy.location('pathname').should('eq', '/clades');
+    cy.visit('http://localhost:3000/auth/logout');
+    cy.location('pathname').should('eq', '/auth/logout');
+    cy.location('pathname', { timeout: 5000 }).should('eq', '/');
   });
 });
--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -1,10 +1,8 @@
-const localhost_and_port = 'http://localhost:3000';
-
 describe('Can view profile page when', function() {
   it('logs in and visits /profile', function() {
     cy.login('admin', 'adminadmin');
-    cy.url().should('eq', localhost_and_port + '/clades');
-    cy.visit(localhost_and_port + '/profile');
-    cy.url().should('eq', localhost_and_port + '/profile');
+    cy.location('pathname').should('eq', '/clades');
+    cy.visit('http://localhost:3000/profile');
+    cy.location('pathname').should('eq', '/profile');
   });
 });
