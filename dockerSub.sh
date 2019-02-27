26baad146bb67dd4724ce2ab036cc4babdf30545
Emmanuel Proulx <emmanuel.proulx@dialogic.com>
2019-02-27 18:00:29 -0500

    Added push for docker image.

--- dockerSub.sh
+++ dockerSub.sh
@@ -6,4 +6,13 @@ cp $1/ecosystem.config.js release/$1
 cp $1/Dockerfile release/$1
 cd release/$1
 docker build -t phylogeny-explorer/$1:0.0.1 .
+if [[ -z "${DOCKER_USERNAME}" ]]; then
+exit 0
+fi
+if [[ -z "${DOCKER_PASSWORD}" ]]; then
+exit 0
+fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push phylogeny-explorer/$1:0.0.1
+
 cd ../..
