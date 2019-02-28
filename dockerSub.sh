52ef378f17571b28298bbffb57e20ce5bd95cd5b
Emmanuel Proulx <emmanuel.proulx@dialogic.com>
2019-02-27 20:33:53 -0500

    typo in docker repo name

--- dockerSub.sh
+++ dockerSub.sh
@@ -6,7 +6,7 @@
 cp $1/ecosystem.config.js release/$1
 cp $1/Dockerfile release/$1
 cd release/$1
-docker build -t phylogeny-explorer/$1:$2 .
+docker build -t phylogenyexplorer/$1:$2 .
 if [[ -z "${DOCKER_USERNAME}" ]]; then
 exit 0
 fi
@@ -14,7 +14,7 @@ if [[ -z "${DOCKER_PASSWORD}" ]]; then
 exit 0
 fi
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker tag master phylogeny-explorer/$1
-docker push phylogeny-explorer/$1:$2
+#docker tag master phylogenyexplorer/$1
+docker push phylogenyexplorer/$1:$2
 
 cd ../..
